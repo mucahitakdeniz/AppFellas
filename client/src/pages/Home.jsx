c5ce748cdd8cc2f96cb9a9f875ec71e4d1505522
Mucahit <mucahitakdeniz28@gmail.com>
2024-09-20 22:55:26 +0300

    homeCSS

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <Box
       sx={{
         height: "100vh",
-        backgroundColor: "#f3e8ff",
+        backgroundColor: "#d1c4e9",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
@@ -21,12 +21,7 @@ const Home = () => {
         height: '78vh',
         padding: '20px', 
         borderRadius: '20px',
-        boxShadow: `
-          0 1px 5px rgba(253, 252, 253, 0.7),
-          2px -2px 4px rgba(247, 244, 246, 0.3),
-          -2px -2px 4px rgba(226, 220, 224, 0.3),
-          0 8px 12px rgba(128, 0, 128, 0.7)
-        `,
+       
       }}>
         <Navbar />
       </Container>
